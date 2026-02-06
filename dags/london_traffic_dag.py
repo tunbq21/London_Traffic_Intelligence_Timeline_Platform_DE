e60d9d24776d6ae3e7e790603918c5f3104c243e
MokaMoka77 <tbuiquang103@gmail.com>
2026-02-06 10:24:35 +0700

    clean data json

--- dags/london_traffic_dag.py
+++ dags/london_traffic_dag.py
@@ -3,16 +3,19 @@ from airflow.operators.python import PythonOperator
 from hooks.tfl_hook import TfLHook
 from datetime import datetime, timedelta
 import json
+import pandas as pd
 
 def extract_bus_data():
     hook = TfLHook()
-    # Gọi endpoint lấy tất cả xe bus
+
     data = hook.get_data("Line/159/Arrivals")   
     
-    # Lưu tạm vào thư mục include để kiểm tra
     file_path = f"/usr/local/airflow/include/bus_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-    with open(file_path, 'w') as f:
-        json.dump(data[:100], f) # Lấy 100 con xe đầu tiên làm mẫu
+    # with open(file_path, 'w') as f:
+    #     json.dump(data[:100], f) 
+    df = pd.DataFrame(data)
+    df_limited = df.head(100)
+    df_limited.to_json(file_path, orient='records', lines=True)
     print(f"Đã trích xuất dữ liệu thành công vào {file_path}")
 
 default_args = {
@@ -25,7 +28,7 @@ default_args = {
 with DAG(
     dag_id='london_bus_extraction_v1',
     default_args=default_args,
-    schedule='@hourly', # Chạy mỗi tiếng một lần
+    schedule='@hourly', 
     catchup=False
 ) as dag:
 
--- plugins/hooks/tfl_hook.py
+++ plugins/hooks/tfl_hook.py
@@ -2,37 +2,6 @@ from airflow.hooks.base import BaseHook
 import requests 
 
 
-# class TfLHook(BaseHook):
-#     """
-#     Hook for interacting with the Transport for London (TFL) API.
-
-#     :param api_key: TFL API key 
-#     :type api_key: str
-#     """ 
-
-#     def __init__(self, api_key: str) -> None:
-#         super().__init__()
-#         self.api_key = api_key
-#         self.base_url = "https://api.tfl.gov.uk" 
-
-#     def get_line_status(self, line_id: str) -> dict:
-#         """
-#         Get the status of a specific TFL line.
-
-#         :param line_id: The ID of the TFL line (e.g., 'victoria', 'central').
-#         :type line_id: str
-#         :return: A dictionary containing the line status information.
-#         :rtype: dict
-#         """
-#         endpoint = f"/Line/{line_id}/Status"
-#         params = {"app_key": self.api_key} 
-#         response = requests.get(f"{self.base_url}{endpoint}", params=params)
-#         response.raise_for_status() 
-#         return response.json() 
-    
-
-
-
 
 class TfLHook(BaseHook):
     def __init__(self, tfl_conn_id='tfl_default'):
@@ -49,5 +18,5 @@ class TfLHook(BaseHook):
         payload['app_key'] = api_key
         
         response = requests.get(base_url, params=payload)
-        response.raise_for_status() # Báo lỗi nếu API gặp sự cố
+        response.raise_for_status()
         return response.json() # Trả về Root (List hoặc Dict)
